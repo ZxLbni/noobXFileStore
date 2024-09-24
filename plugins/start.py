f2c9827fc2783d8a8de5038914633d84efbc79f0
PXZsupport <ghostarmy303@gmail.com>
2024-09-25 02:28:58 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -93,6 +93,13 @@ async def start_command(client: Client, message: Message):
                 [InlineKeyboardButton("✖️ ᴄᴀɴᴄᴇʟ", callback_data = "cancel")],
             ]
                 )
+elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
+        
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
