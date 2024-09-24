20f14a59de10b8bdef543d594160ae90e6a13b89
PXZsupport <ghostarmy303@gmail.com>
2024-09-25 04:02:05 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -43,12 +43,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
         
     elif data == "close":
+        await query.message.delete()
         try:
-            await query.message.delete()
             await query.message.reply_to_message.delete()
-            await query.message.continue_propagation()
         except:
-            await query.message.delete()
-            await query.message.continue_propagation()
             pass
-
