5064ad26a2225362123c829f367aa09cb93a7c41
PXZsupport <ghostarmy303@gmail.com>
2024-10-03 06:32:25 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -128,7 +128,7 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
             InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink2)],
-           [ InlineKeyboardButton(text="ᴊᴏɪɴ ɢʀᴏᴜᴘ", url=client.invitelink3),
+           [ InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
             
             
         ]
