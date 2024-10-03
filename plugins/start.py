38bcc0789a0cd7211552dec52d3cc92793ee19a2
PXZsupport <ghostarmy303@gmail.com>
2024-10-04 03:45:50 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -127,10 +127,10 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
-            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink2)],
-           [ InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
+            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink2)
+           
             
-           ],
+        ]
     ]
     try:
         buttons.append(
